c455b086b65cebba9f13ff27d676224a43a7b6d3
Laura Daniela Estrada Ibarra <daniela.es.ib@gmail.com>
2025-01-08 21:40:15 -0600

    fix favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,6 @@ export default async function RootLayout({
     <html lang={initialLang}>
       <head>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='favicon.ico' type='image/x-icon' />
       </head>
       <body>
         <Providers language={initialLang}>
